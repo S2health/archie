af92ec6b3ab439a687f4dd0dd1dfcaaebd001be3
wolandscat <thomas.beale@graphitehealth.io>
2025-04-17 16:02:42 -0600

    Fix XML annotation error.

--- aom/src/main/java/com/nedap/archie/aom/CAttribute.java
+++ aom/src/main/java/com/nedap/archie/aom/CAttribute.java
@@ -28,7 +28,7 @@ import java.util.List;
 @XmlType(name="C_ATTRIBUTE", propOrder = {
         "existence",
         "differentialPath",
-        "is_multiple",
+        "multiple",
         "cardinality",
         "children"
 })
